7f6c68b136026ba75cbaeb354aeec049be06f009
marlonbze <98763314+marlonbze@users.noreply.github.com>
2022-04-21 13:07:32 -0600

    Add files via upload

---
+++ quizquestions.js
@@ -0,0 +1,117 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Who was elected president of the United States in 2017?",
+    answer: "Donald Trump",
+    options: [
+      "Barrack Obama",
+      "Donald Trump",
+      "George Bush",
+      "Snoop Dog"
+      
+    ]
+  },
+    {
+    numb: 2,
+    question: "When did the Jonas Brothers make their comeback to the world?",
+    answer: "2015",
+    options: [
+      "2019",
+      "2002",
+      "2015",
+      "2008"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the natioal language of Canada?",
+    answer: "English",
+    options: [
+      "Spanish",
+      "Dutch",
+      "French",
+      "English"
+    ]
+  },
+    {
+    numb: 4,
+    question: "In New Zealand what is illegal to fly in a hot air balloon with?",
+    answer: "A dog",
+    options: [
+      "A dog",
+      "A chicken",
+      "A violin",
+      "A Rooster"
+    ]
+  },
+    {
+    numb: 5,
+    question: "If you dug a hole through the center of the earth from Wellington, New Zealand, in which European country would you emerge",
+    answer: "Poland",
+    options: [
+      "Germany",
+      "Poland",
+      "Spain",
+      "France"
+    ]
+  },
+
+  {
+      numb: 6,
+       question: "In the Harry Potter Book and film series, Professor Dumbledore's name comes from the old english word for what creature? ",
+       answer: "Owl",
+       options: [
+         "Unicorn",
+         "Elephant",
+         "Owl",
+         "Shark"
+       ]
+   },
+   {
+    numb: 7,
+     question: "Which month of the year has the longest daylight hours in the Northern Hemisphere and the shortest dalylight in the Southern Hemisphere",
+     answer: "June",
+     options: [
+       "Januray",
+       "May",
+       "August",
+       "June"
+     ]
+ },
+ {
+  numb: 8,
+   question: "Roughly what percentage of people in the world can wiggle their ears?",
+   answer: "20%",
+   options: [
+     "50%",
+     "90%",
+     "30%",
+     "20%"
+   ]
+},
+{
+  numb: 9,
+   question: "What was the first animal sent into space?",
+   answer: "Chimpanzee",
+   options: [
+     "Rabbit",
+     "Dog",
+     "Chimpanzee",
+     "Dragon"
+   ]
+},
+
+{
+  numb: 10,
+   question: "Who was the first person to land on the moon?",
+   answer: "Niel Armstrong",
+   options: [
+     "Hillary Clinton",
+     "Niel Armstrong",
+     "Jeff Besos",
+     "Elon Musk"
+   ]
+},
+
+];
